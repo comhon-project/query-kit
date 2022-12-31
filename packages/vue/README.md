1806ec1d01240deb7472781021cbb873b083d617
jean-philippe <jeanphilippe.perrotton@gmail.com>
2022-12-31 17:04:18 +0100

    [feat] separate core and css in workspaces

---
+++ packages/vue/README.md
@@ -0,0 +1,15 @@
+# query-kit
+
+query-kit is a library for Vue that permit to build complexe queries and display objects collections with an intuitive user interface.
+
+## Run Unit Tests with [Vitest](https://vitest.dev/)
+
+```sh
+npm run test:unit
+```
+
+## Lint with [ESLint](https://eslint.org/)
+
+```sh
+npm run lint
+```
---
+++ packages/vue/index.js
@@ -0,0 +1,14 @@
+import plugin from "./src/core/Plugin";
+import { locale } from './src/i18n/i18n';
+
+import Search from './src/components/Search.vue';
+import Builder from './src/components/Filter/Builder.vue';
+import Collection from './src/components/Collection/Collection.vue';
+
+export {
+    plugin,
+    locale,
+    Search,
+    Builder,
+    Collection,
+}
